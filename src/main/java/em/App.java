92897b26207ae8bacbf13dc18730e4a3e462416c
eddiesong <eddiesong@live.com>
2018-06-07 17:12:47 +0800

    del target out

--- src/main/java/em/App.java
+++ src/main/java/em/App.java
@@ -1,6 +1,12 @@
 package em;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
@@ -11,5 +17,14 @@ public class App
     public static void main(String[] args) {
         Properties properties= java.lang.System.getProperties();
         System.out.println(properties.getProperty("java.version"));
+
+
+        String queryPropertyStr="";
+        Set<String> set= Arrays.stream(queryPropertyStr.split(","))
+                .filter((x)->!StringUtils.isNoneEmpty(x))
+                .map(x->x)
+                .collect(Collectors.toSet());
+
+
     }
 }
